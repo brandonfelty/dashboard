c7139a6825cbe906a353ec629af9178d82bff0c9
Brandon Felty <brcfelty@gmail.com>
2022-10-13 14:59:39 -0600

    add activemenu to context

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -10,9 +10,13 @@ const initialState = {
 }
 
 export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+
   return (
     <StateContext.Provider
-      value={{ test: 'test' }}
+      value={{ 
+        activeMenu, 
+      }}
     >
       { children }
     </StateContext.Provider>
