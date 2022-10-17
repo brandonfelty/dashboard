330844a83b8eb067dc7fdefe840239988393d3db
Brandon Felty <brcfelty@gmail.com>
2022-10-17 11:51:59 -0600

    adds navbar icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -50,6 +50,39 @@ const Navbar = () => {
           color='blue'
           icon={ <FiShoppingCart /> }
         />
+        <NavButton
+          title="Chat"
+          dotColor='#03C9D7'
+          customFunc={() => handleClick('chat')}
+          color='blue'
+          icon={ <FiShoppingCart /> }
+        />
+        <NavButton
+          title="Notifications"
+          dotColor='#03C9D7'
+          customFunc={() => handleClick('notification')}
+          color='blue'
+          icon={ <RiNotification3Line /> }
+        />
+        <TooltipComponent
+          content='Profile'
+          position='BottomCenter'
+        >
+          <div 
+            className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+            onClick={() => handleClick('userProfile')}
+          >
+            <img 
+              src={avatar}
+              className='rounded-full w-8 h-8'
+            />
+            <p>
+              <span className='text-gray-400 text-14'>Hi, </span> {' '}
+              <span className='text-gray-400 font-bold ml-1 text-14'>Dude</span>
+            </p>
+            <MdKeyboardArrowDown className='text-gray-400 text-14'/>
+          </div>
+        </TooltipComponent>
       </div>
     </div>
   )
