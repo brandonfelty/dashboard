299eb1747f1c45833f867408d7079037c47e00a3
Brandon Felty <brcfelty@gmail.com>
2022-10-17 08:42:45 -0600

    add NavButton component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,9 +10,36 @@ import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position='BottomCenter'>
+    <button
+      type='button'
+      onClick={customFunc}
+      style={{ color }}
+      className='relative text-xl rounded-full p-3 hover:bg-ligh-gray'
+    >
+      <span 
+        style={{ background: dotColor }}
+        className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+      >
+        {icon}
+      </span>
+    </button>
+  </TooltipComponent>
+);
+
 const Navbar = () => {
+  const { activeMenu, setActiveMenu } = useStateContext();
+
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prev) => (!prev))}
+        color='blue'
+        icon={ <AiOutlineMenu /> }
+      />
+    </div>
   )
 };
 
