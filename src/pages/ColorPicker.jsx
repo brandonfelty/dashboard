09e27e5a316a7f501d9d6bb2d2fd7f42a39baf74
Brandon Felty <brcfelty@gmail.com>
2022-10-20 07:30:52 -0600

    add color picker preview

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -10,7 +10,15 @@ const ColorPicker = () => {
         category='App'
         title='Color Picker'
       />
-      
+      <div className='text-center'>
+        <div id='preview'/>
+        <div className='flex justify-center items-center gap-20 flex-wrap'>
+          <div>
+            <p className='text-2xl font-semibold mt-2 mb-4'>Inline Pallete</p>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
