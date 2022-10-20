02cb25d6419afc0a9576063dd4607c650412f9ab
Brandon Felty <brcfelty@gmail.com>
2022-10-20 08:35:42 -0600

    add logic for displaying theme settings

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
 
   return (
     <div>
@@ -21,6 +21,7 @@ const App = () => {
                 type="button"
                 className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
                 style={{ background: 'blue', borderRadius: '50%' }}
+                onClick={() => setThemeSettings(true)}
               >
                 <FiSettings />
               </button>
@@ -42,7 +43,7 @@ const App = () => {
               <Navbar />
             </div>
             <div>
-              <ThemeSettings />
+              { themeSettings && <ThemeSettings /> }
 
               <Routes>
                 {/* dashboard */}
