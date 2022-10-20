c2631bd62835cb70436268c0ffaced689c430931
Brandon Felty <brcfelty@gmail.com>
2022-10-20 07:28:43 -0600

    sets up color picker component

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../components';
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p10 bg-white rounded-3xl'>
+      <Header 
+        category='App'
+        title='Color Picker'
+      />
+      
+    </div>
   )
 }
 
