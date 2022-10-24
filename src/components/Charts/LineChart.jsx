a1376628396c26c277f1debe62af3bb0d26408c9
Brandon Felty <brcfelty@gmail.com>
2022-10-24 11:10:11 -0600

    adds imports

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 
 const LineChart = () => {
   return (
