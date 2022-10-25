221dece41f1b78c469d5647866e386bef40c58f1
Brandon Felty <brcfelty@gmail.com>
2022-10-25 08:42:05 -0600

    add financial chart

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Tooltip, Zoom, Logarithmic, Crosshair, HiloSeries } from '@syncfusion/ej2-react-charts';
+
+import { Header } from '../../components'
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 
 const Financial = () => {
+  const { currentMode } = useStateContext();
+  
   return (
-    <div>Financial</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+    <Header  category="Chart" title="Super Stonks"/>
+      <ChartComponent
+      id='financial-chart'
+      height='420px'
+      primaryXAxis={FinancialPrimaryXAxis}
+      primaryYAxis={FinancialPrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable: true, shared: true }}
+      crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 }}}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[DateTime, Tooltip, Crosshair, HiloSeries, Logarithmic, Zoom ]}/>
+      <SeriesCollectionDirective>
+        <SeriesDirective 
+          dataSource={financialChartData}
+          xName='x'
+          yName='low'
+          name='Apple Inc'
+          type='Hilo'
+          low='low'
+          high='high'
+        />
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
+    
   )
 }
 
-export default Financial
+export default Financial;
