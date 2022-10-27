04d9e86ea03a22e1da1232ba56f2f106328fe58c
Brandon Felty <brcfelty@gmail.com>
2022-10-27 07:31:53 -0600

    add pie chart to page

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,37 +1,15 @@
 import React from 'react';
-import { Inject, AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, PieSeries, AccumulationDataLabel, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
 
-import { Header } from '../../components'
-import { pieChartData  } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
+import { pieChartData } from '../../data/dummy';
+import { ChartsHeader, Pie as PieChart } from '../../components';
 
-const Pie = () => {
-  const { currentMode } = useStateContext();
-  
-  return (
-    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-    <Header  category="Chart" title="Cost Breakdown"/>
-      <AccumulationChartComponent
-      id='pie-chart'
-      height='420px'
-      chartArea={{ border: { width: 0 }}}
-      tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-    >
-      <Inject services={[ PieSeries, AccumulationDataLabel, AccumulationTooltip ]}/>
-      <AccumulationSeriesCollectionDirective>
-        <AccumulationSeriesDirective 
-          dataSource={pieChartData}
-          dataLabel={{ visible: true, position: 'Outside',  connectorStyle: {     length: '20px', type: 'Curve' }, name: 'text', font:  {fontWeight: '600' } }}
-          xName='x'
-          yName='y'
-          type='Pie'
-        />
-      </AccumulationSeriesCollectionDirective>
-    </AccumulationChartComponent>
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Pie" title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
     </div>
-    
-  )
-}
+  </div>
+);
 
 export default Pie;
