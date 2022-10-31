a28bbb4fd0dc9aa8d4ce8038932daf7857f8a32f
Brandon Felty <brcfelty@gmail.com>
2022-10-31 10:43:47 -0600

    adds styling to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,10 +10,10 @@ const Cart = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div>
-      <div>
-        <div>
-          <p>Shopping Cart</p>
+    <div className='bg-half-transparent w-full fixed nav-item top-0 right-0'>
+      <div className='float-right h-screen duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8'>
+        <div className='flex justify-between items-center'>
+          <p className='font-semibold text-lg'>Shopping Cart</p>
           <Button 
             icon={<MdOutlineCancel />}
             color='rgb(153, 171, 180'
@@ -25,17 +25,17 @@ const Cart = () => {
         {cartData?.map((item, index) => (
           <div key={index}>
             <div>
-              <div>
+              <div className='flex items-center leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4'>
                 <img className='rounded-lg h-80 w-24' src={item.image} alt=''/>
                 <div>
-                  <p>{item.name}</p>
-                  <p>{item.category}</p>
-                  <div>
-                    <p>{item.price}</p>
-                    <div>
-                      <p><AiOutlineMinus /></p>
-                      <p>0</p>
-                      <p><AiOutlinePlus /></p>
+                  <p className='font-semibold'>{item.name}</p>
+                  <p className='text-gray-600 dark:text-gray-400 text-sm font-semibold'>{item.category}</p>
+                  <div className='flex gap-4 mt-2 items-center'>
+                    <p className='font-semibold text-lg'>{item.price}</p>
+                    <div className='flex items-center border-1 border-r-0 border-color rounded'>
+                      <p className='p-2 border-r-1 dark:border-gray-600 border-color text-red-600'><AiOutlineMinus /></p>
+                      <p className='p-2 border-r-1 border-color dark:border-gray-600'>0</p>
+                      <p className='p-2 border-r-1 border-color dark:border-gray-600 text-green-600'><AiOutlinePlus /></p>
                     </div>
                   </div>
                 </div>
@@ -43,17 +43,17 @@ const Cart = () => {
             </div>
           </div>
         ))}
-        <div>
-          <div>
-            <p>Sub Total</p>
-            <p>$890</p>
+        <div className='mt-3 mb-3'>
+          <div className='flex justify-between items-center'>
+            <p className='text-gray-500 dark:text-gray-200'>Sub Total</p>
+            <p className='font-semibold'>$890</p>
           </div>
-          <div>
-            <p>Total</p>
-            <p>$890</p>
+          <div className='flex justify-between items-center mt-3'>
+            <p className='text-gray-500 dark:text-gray-200'>Total</p>
+            <p className='font-semibold'>$890</p>
           </div>
         </div>
-        <div>
+        <div className='mt-5'>
           <Button 
             color='white'
             bgColor={currentColor}
@@ -67,4 +67,4 @@ const Cart = () => {
   )
 }
 
-export default Cart
+export default Cart;
