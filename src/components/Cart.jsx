f16085af739d51a104d211890c2ce44d95398a38
Brandon Felty <brcfelty@gmail.com>
2022-10-31 10:23:42 -0600

    add cart data

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,7 +10,41 @@ const Cart = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div>Cart</div>
+    <div>
+      <div>
+        <div>
+          <p>Shopping Cart</p>
+          <Button 
+            icon={<MdOutlineCancel />}
+            color='rgb(153, 171, 180'
+            bgHoverColor='light-gray'
+            size='2xl'
+            borderRadius='50%'
+          />
+        </div>
+        {cartData?.map((item, index) => (
+          <div key={index}>
+            <div>
+              <div>
+                <img className='rounded-lg h-80 w-24' src={item.image} alt=''/>
+                <div>
+                  <p>{item.name}</p>
+                  <p>{item.category}</p>
+                  <div>
+                    <p>{item.price}</p>
+                    <div>
+                      <p><AiOutlineMinus /></p>
+                      <p>0</p>
+                      <p><AiOutlinePlus /></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
