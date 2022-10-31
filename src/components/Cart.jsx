21ce9cd28a2cef2b1504f41ae6b57ef730eeef50
Brandon Felty <brcfelty@gmail.com>
2022-10-31 09:50:59 -0600

    adds imports

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+
+import { useStateContext } from '../contexts/ContextProvider';
+import { cartData } from '../data/dummy';
+import { Button } from '.';
 
 const Cart = () => {
+  const { currentColor } = useStateContext();
+
   return (
     <div>Cart</div>
   )
