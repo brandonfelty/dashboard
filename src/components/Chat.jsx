429713cb2f2dd3ea975d6f45d2cc43b03c9d11b0
Brandon Felty <brcfelty@gmail.com>
2022-11-01 10:33:33 -0600

    adds styling to top

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,11 +9,11 @@ const Chat = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div>
-      <div>
-        <div>
-          <p>Messages</p>
-          <button>
+    <div className='nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96'>
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-3'>
+          <p className='font-semibold text-lg dark:text-gray-200'>Messages</p>
+          <button type='button' className='text-white text-xs rounded p-1 px-2 bg-orange'>
             5 New
           </button>
         </div>
