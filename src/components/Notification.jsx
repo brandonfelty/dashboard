ab3f04e8b95a54c78204c575550e75b66bd7bdca
Brandon Felty <brcfelty@gmail.com>
2022-11-01 11:50:07 -0600

    adds the chat data

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -7,9 +7,46 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Notifications = () => {
   const { currentColor } = useStateContext();
-  
+
   return (
-    <div>Notifications</div>
+    <div>
+      <div>
+        <div>
+          <p>Notifications</p>
+          <button> 5 New</button>
+        </div>
+        <Button 
+          icon={<MdOutlineCancel />}
+          color='rgb(153, 171, 180)'
+          bgHoverColor='light-gray'
+          size='2xl'
+          borderRadius='50%'
+        />
+      </div>
+      <div>
+        {chatData.map((item, index) => (
+          <div>
+            <img 
+              src={item.image}
+              alt={item.message}
+            />
+            <div>
+              <p>{item.message}</p>
+              <p>{item.desc}</p>
+            </div>
+          </div>
+        ))}
+        <div>
+          <Button 
+            color='white'
+            bgColor={currentColor}
+            text='See all notifications'
+            borderRadius='10px'
+            width='full'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
