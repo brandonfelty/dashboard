c769db77a852fab747b41cba2f31ed37940d81e4
Brandon Felty <brcfelty@gmail.com>
2022-11-01 11:56:41 -0600

    adds styling

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -9,11 +9,11 @@ const Notifications = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div>
-      <div>
-        <div>
-          <p>Notifications</p>
-          <button> 5 New</button>
+    <div className='nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96'>
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-3'>
+          <p className='font-semibold text-lg dark:text-gray-200'>Notifications</p>
+          <button type='button' className='text-white text-xs rounded p-1 px-2 bg-orange-500'> 5 New</button>
         </div>
         <Button 
           icon={<MdOutlineCancel />}
@@ -23,20 +23,21 @@ const Notifications = () => {
           borderRadius='50%'
         />
       </div>
-      <div>
+      <div className='mt-5'>
         {chatData.map((item, index) => (
-          <div>
+          <div key={index} className='flex items-center leading-8 gap-5 border-b-1 border-color p-3'>
             <img 
+              className='rouned-full h-10 w-10' 
               src={item.image}
               alt={item.message}
             />
             <div>
-              <p>{item.message}</p>
-              <p>{item.desc}</p>
+              <p className='font-semibold dark:text-gray-200'>{item.message}</p>
+              <p className='text-gray-500 text-sm dark:text-gray-400'>{item.desc}</p>
             </div>
           </div>
         ))}
-        <div>
+        <div className='mt-5'>
           <Button 
             color='white'
             bgColor={currentColor}
