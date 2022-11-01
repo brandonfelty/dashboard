be25aab067bc3a9a98f35adfdd7f7b0ed766e6d6
Brandon Felty <brcfelty@gmail.com>
2022-11-01 11:44:20 -0600

    adds all imports

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+
+import { Button } from '.';
+import { chatData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Notifications = () => {
+  const { currentColor } = useStateContext();
+  
   return (
     <div>Notifications</div>
   )
 }
 
-export default Notifications
+export default Notifications;
