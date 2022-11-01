dccd21cb71116b6d75cfa42423bfbce7c5213b63
Brandon Felty <brcfelty@gmail.com>
2022-11-01 10:20:41 -0600

    sets up imports

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+
+import { Button } from '.';
+import { chatData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Chat = () => {
+  const { currentColor } = useStateContext();
+  
   return (
     <div>Chat</div>
   )
 }
 
-export default Chat
+export default Chat;
